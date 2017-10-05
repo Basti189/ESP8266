c0b5728c73924b8091e9576c7b1c5035a1a2b84c
Sebastian Wolf <Basti189@users.noreply.github.com>
2017-10-05 14:16:33 +0200

    Update WifiManager.cpp

--- WifiManager/WifiManager.cpp
+++ WifiManager/WifiManager.cpp
@@ -1,7 +1,4 @@
 #include "WifiManager.h"
-#include "FS.h"
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 
 
 WifiManager::WifiManager() {
@@ -9,24 +6,45 @@ WifiManager::WifiManager() {
 }
 
 String WifiManager::getVersion() {
-  return this->version;
+	return this->version;
 }
 
-int WifiManager::setupWifiManager() {
-	return setupWifiManager(115200);
+String WifiManager::getIP() {
+	return ipAdress;
 }
 
-int WifiManager::setupWifiManager(int baudrate) {
-	Serial.begin(baudrate);
+String WifiManager::getSSID() {
+	return ssid;
+}
+
+void WifiManager::addListener(Callback callback) {
+	this->callback = callback;
+}
+
+void WifiManager::broadcast(byte service, byte state) {
+	String array[] = {};
+	broadcast(service, state, array);
+}
+
+void WifiManager::broadcast(byte service, byte state, String msg[]) {
+	if (callback != nullptr) {
+		callback(service, state, msg);
+	}
+}
+
+uint8_t WifiManager::setup() {
 	if (!readConfig()) { //Konfiguration nicht gefunden
+		broadcast(0x10, 0x01);
 		setupWifi(true);
 		setupWebserver(true);
 		while (configMode) {
+			dnsServer->processNextRequest();
 			serverSetup->handleClient();
 		}
 		delay(500);
 		setupWebserver(false);
 		setupWifi(false);
+		broadcast(0x10, 0x00);
 	}
 	return connectWifi();
 }
@@ -37,40 +55,41 @@ int WifiManager::setupWifiManager(int baudrate) {
  * 2 = Nicht mehr genutzt
  */
 int WifiManager::connectWifi() {
-	Serial.print("[WifiManager] Verbindung zum Netzwerk \"" + ssid + "\" wird hergestellt");
-	char cssid[50];
-	ssid.toCharArray(cssid, 50);
-	char cpassword[100];
-	password.toCharArray(cpassword, 100);
+	String array[] = {ssid};
+	broadcast(0x14, 0x01, array);
 	WiFi.mode(WIFI_STA);
 	yield();
-	WiFi.begin(cssid, cpassword);
-	int count = 0;
+	WiFi.begin(ssid.c_str(), password.c_str());
+	uint8_t count = 0;
+	String points = "";
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(1000);
-		Serial.print(".");
 		count++;
 		if (count == 15) {
-			Serial.println("\n[WifiManager] Verbindung konnte nicht hergestellt werden  ...\n");
+			broadcast(0x14, 0x00);
 			return 1;
 		}
+		points += ".";
+		String array2[] = {points};
+		broadcast(0x14, 0x02, array2);
 	}
-	Serial.println("\n[WifiManager] Verbindung zum Netzwerk \"" + ssid + "\" hergestellt");
-	Serial.print("[WifiManager] IP-Adresse: ");
-	Serial.println(WiFi.localIP());
+	
+	IPAddress lIP = WiFi.localIP();
+	ipAdress = String(lIP[0]) + '.' + String(lIP[1]) + '.' + String(lIP[2]) + '.' + String(lIP[3]);
+	String array2[] = {ssid, ipAdress};
+	broadcast(0x14, 0x03, array2);
 	return 0;
 }
 
 bool WifiManager::readConfig() {
-	Serial.println("[WifiManager] Lade Konfiguration...");
 	SPIFFS.begin();
 	File fConfig = SPIFFS.open(configFile, "r");
 	if (!fConfig) {
-		Serial.println("[WifiManager] Keine Konfiguration gefunden!");
+		broadcast(0x13, 0x00);
 		return false;
 	}
 	String result = "";
-	int count = 0;
+	uint8_t count = 0;
 	while(fConfig.available()) {
 		char c = fConfig.read();
 		if (c != '\n') {
@@ -87,90 +106,108 @@ bool WifiManager::readConfig() {
 		}
 	}
 	fConfig.close();
-	Serial.println("[WifiManager] Konfiguration erfolgreich geladen");
+	broadcast(0x13, 0x01);
 	return true;
 }
 
 void WifiManager::saveConfig() {
-	Serial.println("[WifiManager] Speichere Konfiguration...");
 	SPIFFS.begin();
 	File fConfig = SPIFFS.open(configFile, "w");
 	if (!fConfig) {
-		Serial.println("Speichern fehlgeschlagen!");
+		broadcast(0x13, 0x02);
 		return;
 	}
 	fConfig.println(ssid);
 	fConfig.println(password);
 	fConfig.close();
-	Serial.println("[WifiManager] Konfiguration gespeichert");
+	broadcast(0x13, 0x03);
 }
 
 void WifiManager::deleteConfig() {
-	Serial.println("[WifiManager] Lösche Konfiguration...");
 	SPIFFS.begin();
 	if (SPIFFS.exists(configFile)) {
 		SPIFFS.remove(configFile);
-		Serial.println("[WifiManager] Konfiguration gelöscht");
+		broadcast(0x13, 0x04);
 	} else {
-		Serial.println("[WifiManager] Löschen der Konfiguration fehlgeschlagen!");
+		broadcast(0x13, 0x05);
 	}
-	delay(500);
 }
 
 void WifiManager::setupWifi(bool state) {
 	if (state) {
-		Serial.println("[WifiManager] Zugriffspunkt wird eingerichtet...");
-		WiFi.softAP("Setup", "");
-		Serial.println("[WifiManager] Zugriffspunkt eingerichtet");
+		String genSSID = "ESP-GSP aA1234";//generateSSID();
+		String genPassword = "12345678";//generatePassword();
+		WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 2558, 255, 0));
+		WiFi.softAP(genSSID.c_str(), genPassword.c_str());
 		IPAddress apIP = WiFi.softAPIP();
-		Serial.print("[WifiManager] IP-Adresse: ");
-		Serial.println(apIP);
+		ipAdress = String(apIP[0]) + '.' + String(apIP[1]) + '.' + String(apIP[2]) + '.' + String(apIP[3]);
+		String array[] = {genSSID, genPassword, ipAdress};
+		broadcast(0x11, 0x01, array);
 	} else {
-		Serial.println("[WifiManager] Zugriffspunkt wird abgeschaltet...");
 		WiFi.softAPdisconnect();
 		WiFi.disconnect();
-		if ( WiFi.status() != WL_CONNECTED ) {
-			delay (250);
+		while ( WiFi.status() == WL_CONNECTED ) {
+			yield();
 		}
 		WiFi.mode(WIFI_STA);
-		Serial.println("[WifiManager] Zugriffspunkt abgeschaltet");
+		broadcast(0x11, 0x00);
 	}
+}
+
+String WifiManager::generateSSID() {
+	String genSSID = wifiName + " ";
+	String letters[26] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+	String cLetters[26] = {"A", "B", "C", "D", "E", "F", "g", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+	genSSID += letters[random(0, 26)];
+	genSSID += cLetters[random(0, 26)];
+	genSSID += String(random(1000, 10000));
+	return genSSID;
+}
+
+String WifiManager::generatePassword() {
+	return String(random(10000000, 100000000));
 }
 
 void WifiManager::reset() {
 	deleteConfig();
+	WiFi.disconnect();
+	while (WiFi.status() == WL_CONNECTED) {
+		yield();
+	}
+	WiFi.mode(WIFI_OFF);
 }
 
 void WifiManager::setupWebserver(bool state) {
 	if (state) {
-		Serial.println("[WifiManager] Webserver wird eingerichtet...");
-		serverSetup = new ESP8266WebServer(8080);
-		serverSetup->on("/setup", [=]() {
-			for (uint8_t i = 0; i < serverSetup->args(); i++) {
-				if (serverSetup->argName(i) == "ssid") {
-					ssid = serverSetup->arg(i);
-				} else if (serverSetup->argName(i) == "password") {
-					password = serverSetup->arg(i);
-				}
-			}
-			if (ssid != "" && password != "") {
-				serverSetup->send(200, "text/plain", "OK");
-				Serial.println("[WifiManager] Konfiguration:");
-				Serial.println("  -> SSID: " + ssid);
-				Serial.println("  -> Passwort: " + password);
-				saveConfig();
-				configMode = false;
-				delay(200);
-			} else {
-				serverSetup->send(200, "text/plain", "error");
-			}
-		});
+		dnsServer = new DNSServer();
+		dnsServer->start(DNS_PORT, "*", IPAddress(192, 168, 4, 1));
+		serverSetup = new ESP8266WebServer(80);
+		serverSetup->on("/", std::bind(&WifiManager::handleRoot, this));
+		serverSetup->on("/setup", std::bind(&WifiManager::handleSetup, this));
+		serverSetup->onNotFound(std::bind(&WifiManager::handleRoot, this));
 		serverSetup->begin();
-		Serial.println("[WifiManager] Webserver eingerichtet");
+		broadcast(0x12, 0x01);
 	} else {
-		Serial.println("[WifiManager] Webserver wird abgeschaltet...");
 		serverSetup->stop();
+		dnsServer->stop();
 		delete serverSetup;
-		Serial.println("[WifiManager] Webserver abgeschaltet");
+		delete dnsServer;
+		broadcast(0x12, 0x00);
 	}
+}
+
+void WifiManager::handleRoot() {
+	String page = FPSTR(HTTP_ROOT);
+	serverSetup->sendHeader("Content-Length", String(page.length()));
+	serverSetup->send(200, "text/html", page);
+}
+
+void WifiManager::handleSetup() {
+	ssid = serverSetup->arg("ssid").c_str();
+  	password = serverSetup->arg("password").c_str();
+	String page = FPSTR(HTTP_OK);
+	serverSetup->sendHeader("Content-Length", String(page.length()));
+	serverSetup->send(200, "text/html", page);
+	saveConfig();
+	configMode = false;
 }
